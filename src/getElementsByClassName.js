8f8d96aab1411347b795cf6aa43198cda389bb63
Hack Reactor Student <student@hackreactor.com>
2017-06-26 15:41:13 -0700

    Recursion Topic getElementsByClass   - wrote a function that retrieves classList if it matches className   - checks to see if ther eare children nodes and calls itself recursively

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var nodes = [];
+
+  function getNodes(node) {
+    if(node.classList.contains(className)){
+      nodes.push(node);
+    } 
+  
+    var children = Array.from(node.children);
+
+    for(var i = 0; i < children.length; i++) {
+      getNodes(children[i]);
+    }
+  }
+
+  getNodes(document.body);
+  return nodes;
 };
+
+/*
+  get all the nodes of the html <body>
+  for each node:
+    
+      write a function that checks children for classname.
+      if there is classname, push it to an array;
+    
+    base case: node doesn't have children
+    recursive case: node has children
+
+*/
